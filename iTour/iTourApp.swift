af6a2656fcf25a5c2679210e40f924a5da60761c
Skylowerr <gokceemirhan23@gmail.com>
2026-05-07 10:37:49 +0200

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Emirhan Gökçe on 7.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
